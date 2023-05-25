fd09915c6126eec627d23b753537f5f03ee4f8e2
Jake Goulding <jake.goulding@integer32.com>
2023-05-25 12:31:31 -0400

    Make DicomValue a homogeneous collection instead of heterogeneous
    The previous type allowed for a collection of mixed integers / strings
    / floats, etc. Since the DICOM format doesn't allow for that, we can
    change the type to enforce a single inner type.
    
    We also create our own `Value` enum instead of using
    `rmpv::Value`. This allows us to ignore non-UTF8 strings and integers
    that cannot be represented as i64.

--- src/dicom_json.rs
+++ src/dicom_json.rs
@@ -14,18 +14,48 @@ pub struct Alphabetic {
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(untagged)]
 pub enum DicomValue {
-    Integer(i64),
-    Float(f64),
-    String(String),
-    Alphabetic(Alphabetic),
-    SeqField(DicomJsonData),
+    Integer(Vec<i64>),
+    Float(Vec<f64>),
+    String(Vec<String>),
+    Alphabetic(Vec<Alphabetic>),
+    SeqField(Vec<DicomJsonData>),
+}
+
+impl DicomValue {
+    pub fn is_empty(&self) -> bool {
+        match self {
+            DicomValue::Integer(v) => v.is_empty(),
+            DicomValue::Float(v) => v.is_empty(),
+            DicomValue::String(v) => v.is_empty(),
+            DicomValue::Alphabetic(v) => v.is_empty(),
+            DicomValue::SeqField(v) => v.is_empty(),
+        }
+    }
+
+    #[cfg(test)]
+    pub fn to_string_ref(&self) -> Option<&[String]> {
+        if let DicomValue::String(s) = self {
+            Some(s)
+        } else {
+            None
+        }
+    }
+
+    #[cfg(test)]
+    pub fn to_alphabetic_ref(&self) -> Option<&[Alphabetic]> {
+        if let DicomValue::Alphabetic(s) = self {
+            Some(s)
+        } else {
+            None
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct DicomField {
     #[serde(rename = "Value")]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub value: Option<Vec<DicomValue>>,
+    pub value: Option<DicomValue>,
     #[serde(with = "vr_serialization")]
     pub vr: VR,
     #[serde(rename = "InlineBinary")]
